02cf50890e820494337d8e3a1701e4a3a9363432
Navodit Maheshwari <54903217+Navodit1603@users.noreply.github.com>
2022-04-04 06:58:28 +0000

    Initial commit

---
+++ Menu.java
@@ -0,0 +1,84 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Menu{
+
+  Map<Integer, MenuRow> menu = new HashMap<>();
+
+  public Menu(MenuRow[] rows){
+    int i = 0;
+    for(MenuRow row : rows){
+      menu.put(i++, new MenuRow(row.getChallenge(), row.getAction()));
+    }
+  }
+
+  public MenuRow get(int i) {
+        return menu.get(i);
+    }
+
+  public void print() {
+        for (Map.Entry<Integer, MenuRow> pair : menu.entrySet()) {
+            System.out.println(pair.getKey() + ": " + pair.getValue().getChallenge());
+        }
+    }
+
+  public static void main(String[] args) {
+        Driver.main(args);
+    }
+  
+}
+
+class MenuRow{
+  String challenge;
+  Runnable action;
+
+  public MenuRow(String challenge, Runnable action){
+    this.challenge = challenge;
+    this.action = action;
+  }
+
+  public String getChallenge() {
+        return this.challenge;
+    }
+    public Runnable getAction() {
+        return this.action;
+    }
+
+  public void run() {
+        action.run();
+    }
+
+}
+
+class Driver{
+  public static void main(String[] args){
+    MenuRow[] rows = new MenuRow[]{new MenuRow("Exit", () -> main(null)),
+                                  new MenuRow("Calculator", () -> Calculator.main(null))};
+
+    Menu menu = new Menu(rows);
+    while(true){
+      System.out.println("Navodit's Data Structures Challenge Labs");
+      menu.print();
+
+      try {
+                Scanner sc = new Scanner(System.in);
+                int selection = sc.nextInt();
+
+                // menu action
+                try {
+                    MenuRow row = menu.get(selection);
+                    // stop menu condition
+                    if (row.getChallenge().equals("Exit"))
+                        return;
+                    // run option
+                    row.run();
+                } catch (Exception e) {
+                    System.out.printf("Invalid selection %d\n", selection);
+                }
+            } catch (Exception e) {
+                System.out.println("Not a number");
+            }
+    }
+  }
+}
